ae90467bdb547fae7c52da0438a3abb38fbb2ff0
tomo <sasatotokikimomo@yahoo.co.jp>
2020-01-20 14:01:01 +0000

    tasklist4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_19_092649) do
+ActiveRecord::Schema.define(version: 2020_01_20_104107) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
