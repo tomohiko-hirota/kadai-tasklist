8108319bb4cebceb1a1e5a97e287dc180ce8c18d
tomo <sasatotokikimomo@yahoo.co.jp>
2020-02-10 11:25:57 +0000

    for heroku

--- db/migrate/20200118173136_create_tasks.rb
+++ db/migrate/20200118173136_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
+      t.references :user, foreign_key: true
       t.string :content
       t.string :status
-      t.references :user, foreign_key: true
 
       t.timestamps
     end
